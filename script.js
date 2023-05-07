a7d345154b64a726c1978713727d6da168e3d73c
AdamSpr <aspring9999@gmail.com>
2023-05-08 00:03:26 +0200

    fist commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// function checkPopulation()
+// {
+//     const cityAsked=document.getElementById("field").value;
+//     const api=`https://pl.wikipedia.org//w/api.php?action=query&format=json&titles=${cityAsked}&origin=*&prop=revisions&rvprop=content`;
+//     console.log(api);
+//     fetch(api)
+//         .then(res => res.json())
+//         .then(data => {
+//             let nextDir;
+//             for(let i in data.query.pages)
+//             {
+//                 nextDir=data.query.pages[i];
+//                 break;
+//             }
+//             let JSONdir;
+//             JSONdir=nextDir.revisions[0]["*"];
+ 
+//             const regex = /\|liczba ludności\s+=\s+([\d\s,&nbsp;]+)/;
+//             const match = JSONdir.match(regex);
+//             const population = match ? parseInt(match[1].replace(/\s|,|&nbsp;/g, "")) : null;
+//             console.log(population);
+//             document.getElementById("response").innerHTML=population;
+//         })
+//         .catch(error => console.log(error));
+//     const api2=`https://pl.wikipedia.org/w/api.php?action=parse&page=Lista_powiat%C3%B3w_w_Polsce&format=json&origin=*`;
+//     fetch(api2)
+//         .then(res=>res.json())
+//         .then(data => {
+//             let parser = new DOMParser();
+//             let doc = parser.parseFromString(data.parse.text["*"],"text/html");
+//             let trElems = doc.getElementsByTagName("tr");
+//             console.log(trElems);
+//             const j = Math.floor(Math.random() * 380) + 1;
+//             console.log(j);
+
+//             document.getElementById("powiat").innerHTML=trElems[j].cells[0].innerText;
+//             document.getElementById("miasto_powiatowe").innerHTML=trElems[j].cells[1].innerText;
+//             document.getElementById("rejestracja").innerHTML=trElems[j].cells[2].innerText;
+//             document.getElementById("wojewodztwo").innerHTML=trElems[j].cells[3].innerText;
+
+//         })
+//         .catch(error => console.log(error));
+// }
+
+function checkPopulation()
+{
+    const api2=`https://pl.wikipedia.org/w/api.php?action=parse&page=Lista_powiat%C3%B3w_w_Polsce&format=json&origin=*`;
+    fetch(api2)
+        .then(res=>res.json())
+        .then(data => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(data.parse.text["*"],"text/html");
+            let trElems = doc.getElementsByTagName("tr");
+            console.log(trElems);
+            const j = Math.floor(Math.random() * 380) + 1;
+            console.log(j);
+
+            document.getElementById("powiat").innerHTML=trElems[j].cells[0].innerText;
+            document.getElementById("miasto_powiatowe").innerHTML=trElems[j].cells[1].innerText;
+            document.getElementById("rejestracja").innerHTML=trElems[j].cells[2].innerText;
+            document.getElementById("wojewodztwo").innerHTML=trElems[j].cells[3].innerText;
+            
+            var cityAsked = trElems[j].cells[1].innerText;
+            if(cityAsked=="miasto na prawach powiatu\n")
+            {
+                cityAsked=trElems[j].cells[0].innerText;
+                console.log("witam");
+            }
+
+            return fetch(`https://pl.wikipedia.org//w/api.php?action=query&format=json&titles=${cityAsked}&origin=*&prop=revisions&rvprop=content`);
+        })
+            .then(res => res.json())
+            .then(data => {
+                let nextDir;
+                console.log("tutaj jestem ");
+                for(let i in data.query.pages)
+                {
+                    nextDir=data.query.pages[i];
+                    break;
+                }
+                let JSONdir;
+                JSONdir=nextDir.revisions[0]["*"];
+            
+                const regex = /\|liczba ludności\s+=\s+([\d\s,&nbsp;]+)/;
+                const match = JSONdir.match(regex);
+                const population = match ? parseInt(match[1].replace(/\s|,|&nbsp;/g, "")) : null;
+                console.log(population);
+                document.getElementById("response").innerHTML=population;
+        })
+        .catch(error => console.log(error));
+}
